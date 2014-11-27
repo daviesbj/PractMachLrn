b478d6f61ed7dda8336c315e0d1660e252364168
daviesbj <daviesbj@bp.com>
2014-11-27 22:56:27 +0000

    Submitted, all right FIRST TIME!!!

--- Quiz4.R
+++ Quiz4.R
@@ -69,3 +69,42 @@ confRF$overall[1]
 confGBM$overall[1]
 confLDA$overall[1]
 confComb$overall[1]
+
+## Question 3
+
+set.seed(3523)
+library(AppliedPredictiveModeling)
+data(concrete)
+inTrain = createDataPartition(concrete$CompressiveStrength, p = 3/4)[[1]]
+training = concrete[ inTrain,]
+testing = concrete[-inTrain,]
+librray( elasticnet )
+set.seed(233)
+modLAS <- enet( as.matrix(training[,1:8]), as.matrix(training[,9]), trace = TRUE)
+plot( modLAS, xvar = 'penalty' )
+
+## Question 4
+# theURL = "https://d396qusza40orc.cloudfront.net/predmachlearn/gaData.csv"
+# download.file( url = theURL, destfile = basename( theURL ), method = 'curl' )
+library(lubridate)  # For year() function below
+dat = read.csv( "gaData.csv" )
+training = dat[year(dat$date) < 2012,]
+testing = dat[(year(dat$date)) > 2011,]
+tstrain = ts(training$visitsTumblr)
+library(forecast)
+modTS = bats(tstrain)
+predTS <- forecast( modTS, h=235 )
+plot(predTS)
+1-sum(( testing$visitsTumblr > predTS$upper[,2] )|( testing$visitsTumblr < predTS$lower[,2] ))/length(testing$visitsTumblr)
+
+## Question 5
+set.seed(3523)
+library(AppliedPredictiveModeling)
+data(concrete)
+inTrain = createDataPartition(concrete$CompressiveStrength, p = 3/4)[[1]]
+training = concrete[ inTrain,]
+testing = concrete[-inTrain,]
+set.seed(325)
+modSVM <- svm( CompressiveStrength~., data = training )
+prdSVM <- predict( modSVM, testing )
+sqrt(mean((prdSVM - testing$CompressiveStrength)^2))
